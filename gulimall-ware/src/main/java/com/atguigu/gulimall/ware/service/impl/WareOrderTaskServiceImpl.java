1946e5c24f01e1202625c7ce849c2ef9bfcac496
phamducdat <phamducdat2402@gmail.com>
2024-05-23 15:22:52 +0700

    feat: translate

--- gulimall-ware/src/main/java/com/atguigu/gulimall/ware/service/impl/WareOrderTaskServiceImpl.java
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/service/impl/WareOrderTaskServiceImpl.java
@@ -1,23 +1,21 @@
 package com.atguigu.gulimall.ware.service.impl;
 
+import com.atguigu.common.utils.PageUtils;
+import com.atguigu.common.utils.Query;
+import com.atguigu.gulimall.ware.dao.WareOrderTaskDao;
+import com.atguigu.gulimall.ware.entity.WareOrderTaskEntity;
+import com.atguigu.gulimall.ware.service.WareOrderTaskService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.atguigu.common.utils.PageUtils;
-import com.atguigu.common.utils.Query;
-
-import com.atguigu.gulimall.ware.dao.WareOrderTaskDao;
-import com.atguigu.gulimall.ware.entity.WareOrderTaskEntity;
-import com.atguigu.gulimall.ware.service.WareOrderTaskService;
-
 @Service("wareOrderTaskService")
 public class WareOrderTaskServiceImpl extends ServiceImpl<WareOrderTaskDao, WareOrderTaskEntity> implements WareOrderTaskService {
 
-    // 根据 订单号 查询 库存工作单
+    // Query the inventory work order based on the order number
     @Override
     public WareOrderTaskEntity getOrderTaskByOrderSn(String orderSn) {
         QueryWrapper<WareOrderTaskEntity> wrapper = new QueryWrapper<>();
