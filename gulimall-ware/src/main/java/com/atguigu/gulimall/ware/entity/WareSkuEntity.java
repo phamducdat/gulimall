4d8002621d548953262c00284f7413ae9e8572c8
phamducdat <phamducdat2402@gmail.com>
2024-05-23 16:53:06 +0700

    feat: translate

--- gulimall-ware/src/main/java/com/atguigu/gulimall/ware/entity/WareSkuEntity.java
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/entity/WareSkuEntity.java
@@ -2,47 +2,51 @@ package com.atguigu.gulimall.ware.entity;
 
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-
-import java.io.Serializable;
-import java.util.Date;
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
- * 商品库存
- * 
- * @author zhengyuli
- * @email zli78122@usc.edu
+ * Product Inventory
+ * <p>
+ * Represents the inventory details of a product SKU in a warehouse.
+ *
+ * @autor zhengyuli
  * @date 2020-06-23 00:25:27
  */
 @Data
 @TableName("wms_ware_sku")
 public class WareSkuEntity implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * id
-	 */
-	@TableId
-	private Long id;
-	/**
-	 * sku_id
-	 */
-	private Long skuId;
-	/**
-	 * 仓库id
-	 */
-	private Long wareId;
-	/**
-	 * 库存数
-	 */
-	private Integer stock;
-	/**
-	 * sku_name
-	 */
-	private String skuName;
-	/**
-	 * 锁定库存
-	 */
-	private Integer stockLocked;
+    /**
+     * ID
+     */
+    @TableId
+    private Long id;
 
+    /**
+     * SKU ID
+     */
+    private Long skuId;
+
+    /**
+     * Warehouse ID
+     */
+    private Long wareId;
+
+    /**
+     * Stock quantity
+     */
+    private Integer stock;
+
+    /**
+     * SKU name
+     */
+    private String skuName;
+
+    /**
+     * Locked stock quantity
+     */
+    private Integer stockLocked;
 }
