6253a11141c063e11e1d7ef4c67acc0ddc986a00
phamducdat <phamducdat2402@gmail.com>
2024-05-23 14:54:27 +0700

    feat: translate

--- gulimall-ware/src/main/java/com/atguigu/gulimall/ware/dao/WareSkuDao.java
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/dao/WareSkuDao.java
@@ -8,27 +8,27 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 /**
- * 商品库存
- *
- * @author zhengyuli
- * @email zli78122@usc.edu
- * @date 2020-06-23 00:25:27
+ * Product Inventory
+ * <p>
+ * Author: zhengyuli
+ * Email: zli78122@usc.edu
+ * Date: 2020-06-23 00:25:27
  */
 @Mapper
 public interface WareSkuDao extends BaseMapper<WareSkuEntity> {
 
-    // 更新 商品库存记录
+    // Update product inventory record
     void addStock(@Param("skuId") Long skuId, @Param("wareId") Long wareId, @Param("skuNum") Integer skuNum);
 
-    // 查询 当前sku 的 总库存量
+    // Query the total inventory of the current SKU
     Long getSkuStock(Long skuId);
 
-    // 根据 skuId 查询 该商品在哪些仓库中有库存
+    // Query which warehouses have stock for the product based on skuId
     List<Long> listWareIdHasSkuStock(@Param("skuId") Long skuId);
 
-    // 锁定库存 (根据 商品id、仓库id、需要锁定的商品件数 锁定库存)
+    // Lock stock (lock stock based on product id, warehouse id, and quantity of product to be locked)
     Long lockSkuStock(@Param("skuId") Long skuId, @Param("wareId") Long wareId, @Param("num") Integer num);
 
-    // 解锁库存 (根据 商品id、仓库id、需要解锁的商品件数 解锁库存)
+    // Unlock stock (unlock stock based on product id, warehouse id, and quantity of product to be unlocked)
     void unLockStock(@Param("skuId") Long skuId, @Param("wareId") Long wareId, @Param("num") Integer num);
 }
