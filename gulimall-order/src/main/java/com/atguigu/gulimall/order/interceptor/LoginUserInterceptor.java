10934422a0d7f5b95158612ac73f2743de12a0f3
phamducdat <phamducdat2402@gmail.com>
2024-05-31 11:01:39 +0700

    translate

--- gulimall-order/src/main/java/com/atguigu/gulimall/order/interceptor/LoginUserInterceptor.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/interceptor/LoginUserInterceptor.java
@@ -11,43 +11,43 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * 登录拦截器 - LoginUserInterceptor
+ * Login Interceptor - LoginUserInterceptor
  */
 @Component
 public class LoginUserInterceptor implements HandlerInterceptor {
 
-    // ThreadLocal : 同一个线程共享数据
-    // 将 MemberResponseVO对象 存入 ThreadLocal 中
+    // ThreadLocal: Data shared by the same thread
+    // Store MemberResponseVO object in ThreadLocal
     public static ThreadLocal<MemberResponseVO> loginUser = new InheritableThreadLocal<>();
 
     /**
-     * 在目标方法执行之前拦截
+     * Intercept before the target method is executed
      */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
         String uri = request.getRequestURI();
         AntPathMatcher antPathMatcher = new AntPathMatcher();
-        // 放行请求 : 根据 订单号 查询 订单信息
+        // Allow request: Query order information by order number
         boolean statusMatch = antPathMatcher.match("/order/order/status/**", uri);
-        // 放行请求 : 支付完成后 支付宝异步回调
+        // Allow request: Alipay asynchronous callback after payment completion
         boolean payedMatch = antPathMatcher.match("/payed/notify", uri);
-        // 放行请求 : 查询支付宝支付状态
+        // Allow request: Query Alipay payment status
         boolean queryPayMatch = antPathMatcher.match("/queryPayStatus", uri);
 
         if (statusMatch || payedMatch || queryPayMatch) {
-            // 放行
+            // Allow
             return true;
         }
 
         MemberResponseVO loginMember = (MemberResponseVO) request.getSession().getAttribute(AuthServerConstant.LOGIN_USER);
         if (loginMember != null) {
-            // 用户已登录 -> 放行
-            // 将 loginMember 存入 ThreadLocal 中
+            // User has logged in -> Allow
+            // Store loginMember in ThreadLocal
             loginUser.set(loginMember);
             return true;
         } else {
-            // 用户未登录 -> 跳转到登录页
-            request.getSession().setAttribute("msg", "请先登录！");
+            // User not logged in -> Redirect to login page
+            request.getSession().setAttribute("msg", "Please log in first!");
             response.sendRedirect("http://auth.gulimall.com/login.html");
             return false;
         }
